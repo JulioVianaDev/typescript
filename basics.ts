dd2c5bddd38148c08b5e603e580a13772d103d19
juliovianadev <juliovianadev@gmail.com>
2023-04-08 16:25:51 -0300

    object types

---
+++ basics.ts
@@ -0,0 +1,22 @@
+let age: number;
+
+age = 13
+
+let names: string;
+
+let arrays: string[];
+
+let person: {
+  name: string,
+  age: number,
+  // isEmployed: boolean,
+};
+
+person={
+  name:'Max',
+  age: 123
+}
+
+// person = {
+//   isEmployed: true,
+// }
